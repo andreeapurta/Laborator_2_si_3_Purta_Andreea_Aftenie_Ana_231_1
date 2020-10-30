4d6d2749406618be30e6599b5256d2108b653a02
andreeapurta <andreea.purta@gmail.com>
2020-10-30 13:47:13 +0200

    v2

--- Laborator_2_Purta_Andreea_Aftenie_Ana_231_1/Program.cs
+++ Laborator_2_Purta_Andreea_Aftenie_Ana_231_1/Program.cs
@@ -15,9 +15,13 @@ namespace Laborator_2_Purta_Andreea_Aftenie_Ana_231_1
             var production = new List<(string, string)>();
             string[,] TS = new string[100, 100];
             string[,] TA = new string[100, 100];
-            string sirInitial = " ";
+            string sirInitial = "";
+            string cuvIntrare = "";
             int productionLength;
-            int linesTA = 0, colTA = 0, linesTS, colTS;
+            int linesTA = 0, colTA = 0, linesTS = 0, colTS = 0;
+            int indexLine = 0;
+            int indexCol = 0;
+            string value = "";
             //citire fisier
 
             using (StreamReader fisier = new StreamReader(@"C:\Users\Andreea Purta\Desktop\Laborator_2_Purta_Andreea_Aftenie_Ana_231_1\Laborator_2_Purta_Andreea_Aftenie_Ana_231_1\textFile.txt"))
@@ -34,25 +38,98 @@ namespace Laborator_2_Purta_Andreea_Aftenie_Ana_231_1
 
                 for (int i = 0; i < linesTA; i++)
                 {
+                    string line = fisier.ReadLine();
+                    string[] terms = line.Split(" ");
                     for (int j = 0; j < colTA; j++)
                     {
-                        TA[i, j] = ((char)fisier.Read()).ToString();
-
+                        TA[i, j] = terms[j];
                     }
                 }
+
+                Int32.TryParse(fisier.ReadLine(), out linesTS);
+                Int32.TryParse(fisier.ReadLine(), out colTS);
+
+                for (int i = 0; i < linesTS; i++)
+                {
+                    string lineTS = fisier.ReadLine();
+                    string[] termsTS = lineTS.Split(" ");
+                    for (int j = 0; j < colTS; j++)
+                    {
+                        TS[i, j] = termsTS[j];
+                    }
+                }
+
+                cuvIntrare += fisier.ReadLine();
+
             }
+
+            //afisari
+
             Console.WriteLine(nonterminals);
             Console.WriteLine(terminals);
-            Console.WriteLine("Amu afisam matricea");
+            Console.WriteLine("Amu afisam matricea TA");
             for (int i = 0; i < linesTA; i++)
             {
                 for (int j = 0; j < colTA; j++)
                 {
-                    Console.Write(string.Format("{0} ", TA[i, j]));
+                    Console.Write(TA[i, j] + "\t");
 
                 }
+                Console.WriteLine();
             }
+            Console.WriteLine("Amu afisam matricea TS");
+            for (int i = 0; i < linesTS; i++)
+            {
+                for (int j = 0; j < colTS; j++)
+                {
+                    Console.Write(TS[i, j] + "\t");
 
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("Amu afisam cuvant intrari" + cuvIntrare);
+
+            //initializare stiva cu $ si 0
+            stack.Push('0');
+            stack.Push('$');
+        
+            for (int i = 0; i < cuvIntrare.Length; i++)
+            {
+
+                indexLine = 0;
+                indexCol = cuvIntrare.IndexOf(cuvIntrare[0]);
+                value = TA[indexLine, indexCol];
+
+                if (value[0] == 'd')
+                {
+                    stack.Push(cuvIntrare[0]);
+                    stack.Push(value[1]);
+                }
+
+                else if (value[0] == 'r')
+                {
+                    stack.Pop();
+                    stack.Pop();
+                    stack.Push(production[value[1]].Item1);
+                    stack.Push(TS[indexLine, nonterminals.IndexOf(production[value[1]].Item1)]);
+                    Console.WriteLine("Amu Iar stiva");
+                    foreach (var item in stack)
+                    { Console.Write(item + ","); }
+                }
+         
+                else if (value == "acc")
+                {
+                    Console.WriteLine("Acceptare");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Eroare");
+                    break;
+                }
+
+            }
+           
 
         }
     }
